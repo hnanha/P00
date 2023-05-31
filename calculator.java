6ddb418a19b082d72d99fd560d6e06be319b6930
hnanha <135148440+hnanha@users.noreply.github.com>
2023-05-31 23:55:06 +0800

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,28 @@
+/**
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * 22030807, 30 May 2023 2:49:29 pm
+ */
+
+/**
+ * @Hannah 22030807
+ *
+ */
+public class calculator {
+	public int add(int a, int b) { 
+		return a + b;
+	}
+	public int subtract (int a, int b) { 
+		return a-b;
+	}	
+	public int multiple (int a, int b) {
+		return a*b;
+	}
+	public int divide (int a, int b) {
+		return a/b;
+	}
+
+
+}
